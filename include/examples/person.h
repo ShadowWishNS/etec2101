db09be018106d58b043af285fe042dba5de15e08
Daniel G <150769031+ShadowWishNS@users.noreply.github.com>
2025-01-22 12:58:54 -0500

    Sync from hard storage

--- include/examples/person.h
+++ include/examples/person.h
@@ -5,12 +5,28 @@
 
 class Person {
 	//Attributes -- variable for all instances of the class
-public:
+private:
 	int id;
 	std::string first_name;
 	std::string last_name;
 	float hourly_rate;
 	unsigned int hours_worked;
+public:
+	//Constructor
+	Person();
+	Person(int starting_id, std::string starting_fname, std::string starting_lname);
+
+	//GET
+	int get_id();
+	std::string get_firstname();
+	std::string get_lastname();
+	float get_hourly_rate();
+	unsigned int get_hours_worked();
+
+	//SET
+	void set_hourly_rate();
+	void set_hours_worked();
+
 
 	//METHOD -- a function that instances of the class can call
 	float calculate_wage();
--- src/examples/person.cpp
+++ src/examples/person.cpp
@@ -1,5 +1,23 @@
 #include <person.h>
 
+Person::Person()
+{
+	id = -1;
+	first_name = ? ;
+	last_name = ? ;
+	hourly_rate = 0.0f;
+	hours_worked = 0;
+
+}
+
+Person::Person(int start_id, std::string start_fname, std::string start_lname) 
+{
+	id = start_id;
+	first_name = start_fname ;
+	last_name = start_lname ;
+	hourly_rate = 0.0f;
+	hours_worked = 0;
+}
 
 //We are providing the BODY of the method we declared in our header file
 float Person::calculate_wage() {
@@ -7,4 +25,27 @@ float Person::calculate_wage() {
 	// instance. Our job in this method is to tell the compiler HOW to
 	// calulate a wage using our attributes.
 	return hourly_rate * hours_worked;
+}
+
+int get_id() {
+	return id;
+}
+std::string get_firstname() {
+	return first_name;
+}
+std::string get_lastname() {
+	return last_name;
+}
+float get_hourly_rate() {
+	return hourly_rate;
+}
+unsigned int get_hours_worked() {
+	return hours_worked;
+}
+
+void set_hourly_rate(float new_rate) {
+	hourly_rate = new_rate;
+}
+void set_hours_worked(unsigned int new_hours) {
+	hours_worked = new_hours;
 }
